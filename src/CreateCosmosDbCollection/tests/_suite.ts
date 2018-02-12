32cc78d933857c93e238b03388e3d345beddda98
John Downs <john@johndowns.co.nz>
2018-02-13 07:03:38 +1100

    Tidy up

--- src/CreateCosmosDbCollection/tests/_suite.ts
+++ src/CreateCosmosDbCollection/tests/_suite.ts
@@ -2,7 +2,7 @@ import * as path from 'path';
 import * as assert from 'assert';
 import * as ttm from 'vsts-task-lib/mock-test';
 
-describe('Sample task tests', function () {
+describe('CreateCosmosDbCollection task tests', function () {
     before(() => {
     });
 
@@ -68,24 +68,4 @@ describe('Sample task tests', function () {
 
         done();
     });
-
-    /*
-    it('it should fail if tool returns 1', (done: MochaDone) => {
-        this.timeout(1000);
-
-        let tp = path.join(__dirname, 'failrc.js');
-        let tr: ttm.MockTestRunner = new ttm.MockTestRunner(tp);
-
-        tr.run();
-        assert(!tr.succeeded, 'should have failed');
-        assert.equal(tr.invokedToolCount, 1);
-        assert.equal(tr.warningIssues, 0, "should have no warnings");
-        assert.equal(tr.errorIssues.length, 1, "should have 1 error issue");
-        assert.equal(tr.errorIssues[0], '/mocked/tools/echo failed with return code: 1', 'error issue output');
-        assert(tr.stdout.indexOf('atool output here') >= 0, "tool stdout");
-        assert.equal(tr.stdout.indexOf('Hello Mock!'), -1, "task module should have never been called");
-
-        done();
-    });
-    */
 });
