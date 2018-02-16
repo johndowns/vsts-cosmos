6291ca61ae32b9297997c0735962afb247bf1766
John Downs <john@johndowns.co.nz>
2018-02-17 10:31:05 +1100

    Update logic to handle promise resolution

--- src/CreateCosmosDbCollection/azureRm.ts
+++ src/CreateCosmosDbCollection/azureRm.ts
@@ -24,10 +24,19 @@ export async function getCosmosDbAccountKey(
                 console.log("4");
                 var client = new CosmosDbManagementClient(credentials, subscriptionId);
                 console.log("5");
-                var keyResponse = await client.databaseAccounts.listKeys(resourceGroupName, accountName);
-                console.log("6");
-                resolve(keyResponse.primaryMasterKey);
-                console.log("7");
+                try {
+                    console.log("6");
+                    var keyResponse = await client.databaseAccounts.listKeys(resourceGroupName, accountName);
+                    console.log("7");
+                    resolve(keyResponse.primaryMasterKey);
+                    console.log("8");
+                }                
+                catch (err) {
+                    console.log("9");
+                    reject(err);
+                    console.log("10");
+                }
+                console.log("11");
             }
         );
     });
