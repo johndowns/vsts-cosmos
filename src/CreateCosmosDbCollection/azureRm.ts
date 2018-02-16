3894c18a12a5f4304be30f381af5e61da30e3552
John Downs <john@johndowns.co.nz>
2018-02-17 08:14:41 +1100

    Add more logging

--- src/CreateCosmosDbCollection/azureRm.ts
+++ src/CreateCosmosDbCollection/azureRm.ts
@@ -8,18 +8,24 @@ export async function getCosmosDbAccountKey(
     tenantId: string,
     subscriptionId: string, 
     resourceGroupName: string, 
-    accountName: string): Promise<string> {
+    accountName: string)
+    : Promise<string> {
 
+    console.log("1");
     return new Promise<string>(function(resolve, reject) {
+        console.log("2");
         MsRest.loginWithServicePrincipalSecret(clientId, clientSecret, tenantId,
             async (err, credentials) => {
                 if (err) {
+                    console.log("3");
                     reject(err);
                 }
     
+                console.log("4");
                 var client = new CosmosDbManagementClient(credentials, subscriptionId);
                 var keyResponse = await client.databaseAccounts.listKeys(resourceGroupName, accountName);
                 resolve(keyResponse.primaryMasterKey);
+                console.log("5");
             }
         );
     });
