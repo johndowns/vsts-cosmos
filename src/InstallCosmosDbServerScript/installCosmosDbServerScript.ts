8815a3cc56fe4545d3e4ad67949d627b54e98ccb
John Downs <john@johndowns.co.nz>
2018-02-18 17:37:45 +1100

    Add file encoding support

--- src/InstallCosmosDbServerScript/installCosmosDbServerScript.ts
+++ src/InstallCosmosDbServerScript/installCosmosDbServerScript.ts
@@ -5,6 +5,12 @@ import * as azureRm from './azureRm'
 import fs = require('fs');
 import iconv = require('iconv-lite');
 
+const ENCODING_ASCII: string = 'ascii';
+const ENCODING_UTF_7: string = 'utf-7';
+const ENCODING_UTF_8: string = 'utf-8';
+const ENCODING_UTF_16LE: string = 'utf-16le';
+const ENCODING_UTF_16BE: string = 'utf-16be';
+
 async function run() {
     try {
         // get the inputs
@@ -59,13 +65,44 @@ async function run() {
 }
 
 async function installCosmosDbServerScript(accountName: string, accountKey: string, databaseId: string, collectionId: string, scriptId: string, scriptFilePath: string, scriptType: string, triggerType: string, triggerOperation: string) {
-    let scriptFileContents: string = iconv.decode(fs.readFileSync(scriptFilePath), 'auto');
-    
+    let scriptFileContents: string = iconv.decode(fs.readFileSync(scriptFilePath), getEncoding(scriptFilePath));
+
     if (scriptType == "udf") {
         console.log(`Installing user-defined function '${scriptId}' into collection '${collectionId}'...`)
         await cosmos.createUdfAsync(accountName, accountKey, databaseId, collectionId, scriptId, scriptFileContents);
     } else {
         throw new Error('TODO not supported type');
+    }
+}
+
+var getEncoding = function (filePath: string): string {
+    // thanks to https://github.com/qetza/vsts-replacetokens-task/blob/master/task/index.ts
+    let fd: number = fs.openSync(filePath, 'r');
+
+    try
+    {
+        let bytes: Buffer = new Buffer(4);
+        fs.readSync(fd, bytes, 0, 4, 0);
+
+        let encoding: string = ENCODING_ASCII;
+        if (bytes[0] === 0x2b && bytes[1] === 0x2f && bytes[2] === 0x76 && (bytes[3] === 0x38 || bytes[3] === 0x39 || bytes[3] === 0x2b || bytes[3] === 0x2f))
+            encoding = ENCODING_UTF_7;
+        else if (bytes[0] === 0xef && bytes[1] === 0xbb && bytes[2] === 0xbf)
+            encoding = ENCODING_UTF_8
+        else if (bytes[0] === 0xfe && bytes[1] === 0xff)
+            encoding = ENCODING_UTF_16BE
+        else if (bytes[0] === 0xff && bytes[1] === 0xfe)
+            encoding = ENCODING_UTF_16LE
+        else
+            task.debug('BOM no found: default to ascii.');
+
+        task.debug('encoding: ' + encoding);
+
+        return encoding;
+    }
+    finally
+    {
+        fs.closeSync(fd);
     }
 }
 
